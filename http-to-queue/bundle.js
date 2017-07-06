0b77cede90453f1c171502cd9ad4b028edce6b55
Rick Love <rick@toldpro.com>
2017-07-06 13:09:57 -0500

    TEST Http to Queue

--- http-to-queue/bundle.js
+++ http-to-queue/bundle.js
@@ -122,7 +122,7 @@ exports.createFunctionJson = createFunctionJson;
 function runFunction(config, context, req) {
     const data = config.getDataFromRequest(req, context.bindingData);
     context.bindings.outOutputQueue = data;
-    context.log('The Data was Queued', data);
+    // context.log('The Data was Queued', data);
     context.res = {
         body: 'The Data was Queued'
     };
